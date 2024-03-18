b08a2a81311df33a351941dcdc866ffd0fd0b173
abengtss-max <ali_bengtsson@icloud.com>
2024-03-19 00:58:39 +0100

    fixing virtual networking

--- ado-agent/modules/virtual_network/main.tf
+++ ado-agent/modules/virtual_network/main.tf
@@ -11,13 +11,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
-
-data "azurerm_subscription" "current" {}
-
-resource "azurerm_virtual_network_peering" "peering" {
-  name                      = var.peering_name
-  resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.vnet.name
-  remote_virtual_network_id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.remote_resource_group_name}/providers/Microsoft.Network/virtualNetworks/AksVNet"
-  allow_virtual_network_access = true
-}
