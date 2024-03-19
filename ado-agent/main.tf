f4703e8e2be8c9bf8205f3b553ec6ed9e975cbe7
abengtss-max <ali_bengtsson@icloud.com>
2024-03-19 04:08:31 +0100

    minor typo fix

--- ado-agent/main.tf
+++ ado-agent/main.tf
@@ -40,7 +40,7 @@ resource "random_integer" "ri" {
 
 module "storage_account" {
   source                  = "./modules/storage_account"
-  storage_account_name    = "tfstatet${random_integer.ri.result}"
+  storage_account_name    = "tfstate${random_integer.ri.result}"
   resource_group_name     = module.resource_group.name
   location                = var.location
   account_tier            = "Standard"
