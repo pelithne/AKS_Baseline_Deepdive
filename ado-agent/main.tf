05b19c7e740ea1df06d1c240d11a5485244fcda8
abengtss-max <ali_bengtsson@icloud.com>
2024-03-19 03:52:09 +0100

    storage account terraform

--- ado-agent/main.tf
+++ ado-agent/main.tf
@@ -41,8 +41,8 @@ resource "random_integer" "ri" {
 module "storage_account" {
   source                  = "./modules/storage_account"
   storage_account_name    = "tfstatet${random_integer.ri.result}"
-  resource_group_name     = azurerm_resource_group.rg.name
-  location                = azurerm_resource_group.rg.location
+  resource_group_name     = module.resource_group.name
+  location                = module.resource_group.location
   account_tier            = "Standard"
   account_replication_type = "GRS"
 }
